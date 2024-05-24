c686c14c5cce4e935886a0d64f67c7de237e2ce8
Raven Alexander <theravenalexander@gmail.com>
2024-05-23 21:00:24 -0600

    setting state and effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import PokemonList from './PokemonList';
+import axios from 'axios'
 
 function App() {
-  const [pokemon, setPokemon] = useState();
+  const [pokemon, setPokemon] = useState([]);
+  const [currentPageUrl, setCurrentPageUrl] = useState('https://pokeapi.co/api/v2/pokemon/')
+
+  useEffect(() => {
+    
+      axios.get(currentPageUrl).then(res => {
+        setPokemon(res.data.results.map(p => p.name))
+      })
+
+  }, [currentPageUrl]) 
 
   return (
     <>
-      Hi
+     <PokemonList pokemon={pokemon} />
     </>
   )
 }
